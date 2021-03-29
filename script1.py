4b1630d3a896af0d8e1e7b3123bd212e429aed4b
gr006 <levchenko.vr@phystech.edu>
2021-03-29 16:56:55 +0300

    first script

---
+++ script1.py
@@ -0,0 +1,38 @@
+import RPi.GPIO as GPIO
+import time
+from test import decToBinList
+
+
+GPIO.setmode(GPIO.BCM)
+leds = [10, 9, 11, 5, 6, 13, 19, 26]
+
+
+def main():
+    while True:
+        print("Введите значение от 0 до 255(-1 для выхода): ")
+        try:
+            value = int(input())
+        except ValueError:
+            print("Некорректное значение. Попробуйте ещё раз.")
+            continue
+        if value == -1:
+            GPIO.cleanup()
+            exit()
+        elif 0 <= value <= 255:
+            break
+    num2dac(value)
+    print("Для продолжения нажмите Enter, для выхода -1: ")
+    while input() != '-1':
+        main()
+    
+
+def num2dac(value):
+    dac = [leds[x] for x in range(8) if decToBinList(value)[x] == 1]
+    GPIO.setup(dac, GPIO.OUT)
+    GPIO.output(dac, 1)
+    time.sleep(10)
+    GPIO.output(dac, 0)
+
+
+if __name__ == "__main__":
+    main()
